332f709784a1eb8b66d0beafec7ddf4b25ba573f
Scott Rallya <srallya@gmail.com>
2019-02-04 11:32:15 -0500

    Adding bootstrap support; simple display of EC2 instances

--- aws-monitor/aws-monitor.py
+++ aws-monitor/aws-monitor.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 from flask_admin import Admin
+from flask_bootstrap import Bootstrap
+from flask_assets import Environment, Bundle
 
 import boto3
 import json
@@ -9,6 +11,14 @@ app = Flask(__name__)
 app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
 
 admin = Admin(app, name='aws-monitor', template_mode='bootstrap3')
+assets = Environment(app)
+assets.url = app.static_url_path
+
+scss = Bundle('scss/main.scss', filters='pyscss', output='main.css')
+assets.register('scss_all', scss)
+
+Bootstrap(app)
+
 
 @app.route('/')
 def hello():
